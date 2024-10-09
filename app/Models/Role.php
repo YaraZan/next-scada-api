8a4c1c6c7d061a2300e47fa86f561c730843f510
andrei_potiomkin <89867134953@mail.ru>
2024-10-09 13:28:18 +0300

    feat(models): (#1) add name as fillable in role model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -10,6 +10,10 @@ class Role extends Model
 {
     use HasFactory, HasUuid;
 
+    protected $fillable = [
+        'name'
+    ];
+
     public function users()
     {
         return $this->hasMany(User::class);
