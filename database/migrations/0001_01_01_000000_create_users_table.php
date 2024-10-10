165ac75ac7be4039d36db4c06a5dade4367ec035
andrei_potiomkin <89867134953@mail.ru>
2024-10-10 15:38:17 +0300

    feat(database): (#1) write migrations for roles and users tables

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->binary('uuid')->unique();
+            $table->binary('uuid', 16)->unique();
             $table->foreignId('role_id')->nullable()->index();
             $table->string('name');
             $table->string('email')->unique();
--- database/migrations/2024_10_08_134402_create_roles_table.php
+++ database/migrations/2024_10_08_134402_create_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->binary('uuid')->unique();
+            $table->binary('uuid', 16)->unique();
             $table->string('name');
             $table->timestamps();
         });
