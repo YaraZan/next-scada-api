02dd116619dae8da7b52d8cf3322b8c722ab008b
andrei_potiomkin <89867134953@mail.ru>
2024-11-07 15:41:46 +0300

    feat: (#8) register view from ws member role gate rule

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Policies\MemberRolePolicy;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        /** Member roles */
+        Gate::define('viewFromWorkspace-memberRole', [MemberRolePolicy::class, 'viewFromWorkspace']);
     }
 }
